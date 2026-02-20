9febfdbd497211e547af4f3f8d47ee7d47de0330
Mohammed Baahir Yusuf <mby8411@.rit.edu>
2026-02-20 21:54:50 +0400

    Modified to commutePlan attribute to enum type (For pre-dfined Fixed Values)

--- src/main/java/org/example/tigerboard/model/Student.java
+++ src/main/java/org/example/tigerboard/model/Student.java
@@ -5,10 +5,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class Student {
 
+    /*
+    Enums representing the valid commute plan options for students.
+    Restricting commutePlan to predefined constant values only
+     */
+    public enum CommutePlan {
+        MORNING_ONLY,   // Student commutes in the morning only
+        EVENING_ONLY,   // Student commutes in the evening only
+        ROUND_TRIP      // Student commutes both morning and evening
+    }
+
     //Attributes
     private String name;
     private int userId; // References Student User from User class
-    private String commutePlan; // "MORNING_ONLY", "EVENING_ONLY", "ROUND_TRIP"
+    private CommutePlan commutePlan; // Restricted to CommutePlan enum values
     private String location;
 
 
@@ -25,11 +35,11 @@ public class Student {
         this.userId = userId;
     }
 
-    public String getCommutePlan() {
+    public CommutePlan getCommutePlan() {
         return commutePlan;
     }
 
-    public void setCommutePlan(String commutePlan) {
+    public void setCommutePlan(CommutePlan commutePlan) {
         this.commutePlan = commutePlan;
     }
 
