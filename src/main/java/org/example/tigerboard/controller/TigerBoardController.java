0b3911d5f0e2b986229aef6f6204ba5cccbe6617
PrasanthRIT <ps7850@g.rit.edu>
2026-03-15 02:59:20 +0400

    Added controller action to display all buses and added controller action to add new buses

--- src/main/java/org/example/tigerboard/controller/TigerBoardController.java
+++ src/main/java/org/example/tigerboard/controller/TigerBoardController.java
@@ -1,12 +1,18 @@
     package org.example.tigerboard.controller;
 
+    import org.example.tigerboard.model.Bus;
     import org.example.tigerboard.model.Driver;
     import org.example.tigerboard.model.Student;
     import org.example.tigerboard.service.TigerBoardService;
     import org.springframework.stereotype.Controller;
     import org.springframework.web.bind.annotation.GetMapping;
     import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.PathVariable;
     import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestParam;
+
+    import java.util.ArrayList;
+    import java.util.List;
 
     @Controller
     public class TigerBoardController {
@@ -41,8 +47,8 @@
             model.addAttribute("StdList", this.tigerBoardService.getAllStudents());
             return "redirect:students";
         }
-        //Driver
 
+        //Driver
         @GetMapping("/drivers")
         public String getDrivers(Model model){
             model.addAttribute("driverList", this.tigerBoardService.getAllDrivers());
@@ -62,10 +68,62 @@
             return "redirect:/drivers";
         }
 
+        //Buses
+        //Fetches all busses to be displayed on buses template
+        @GetMapping("/buses")
+        public String getBuses(Model model){
+            model.addAttribute("busList", this.tigerBoardService.getAllBuses());
+            return "buses";
+        }
 
+        //Fetches all students and drivers to be displayed on the add-bus template
+        @GetMapping("/buses/add")
+        public String getBusForm(Model model){
+            model.addAttribute("allStudents", this.tigerBoardService.getAllStudents());
+            model.addAttribute("allDrivers", this.tigerBoardService.getAllDrivers());
+            return "add-bus";
+        }
 
+        //Adds a bus with students and drivers based on the IDs selected by the User
+        @PostMapping("/buses/add")
+        public String saveBus(Bus bus, @RequestParam ArrayList<Integer> studentsId, @RequestParam ArrayList<Integer> driversId){
 
+            addStudentsToBus(bus, studentsId);
+            addDriversToBus(bus, driversId);
 
+            this.tigerBoardService.saveBus(bus);
+
+            return "redirect:/add/success/bus";
+        }
+
+        //Routes to success page after adding a bus
+        @GetMapping("/buses/add/success/{entityName}")
+        public String showSuccessPage(Model model, @PathVariable String entityName) {
+            model.addAttribute("entity", entityName);
+            return "success";
+        }
+
+        //Adds all students to bus according to their IDs
+        private void addStudentsToBus(Bus bus, ArrayList<Integer> studentsId) {
+            for (int i = 0; i < studentsId.size(); i++) {
+                Integer tempStudentId = studentsId.get(i);
+                Student student = this.tigerBoardService.getStudentById(tempStudentId);
+                if (student != null) {
+                    bus.getStudents().add(student);
+                }
+            }
+        }
+
+        //Adds all drivers to bus according to their IDs
+        private void addDriversToBus(Bus bus, ArrayList<Integer> driversId) {
+            for (int i = 0; i < driversId.size(); i++) {
+                Integer tempDriverId = driversId.get(i);
+                Driver driver = this.tigerBoardService.getDriverById(tempDriverId);
+                if (driver != null) {
+                    bus.getDrivers().add(driver);
+                }
+            }
+        }
 
     }
 
