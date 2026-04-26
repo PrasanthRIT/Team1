3616aaf2b3c86bf7e6b2de56311c0a7bef8454ed
Manognya Kumar Dandala <manognyadandala@gmail.com>
2026-04-26 19:35:28 +0400

    Integrated again into Baahir

--- src/main/java/org/example/tigerboard/model/User.java
+++ src/main/java/org/example/tigerboard/model/User.java
@@ -1,5 +1,11 @@
 package org.example.tigerboard.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
